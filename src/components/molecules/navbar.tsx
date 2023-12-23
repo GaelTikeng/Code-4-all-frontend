679bc3cf1646269b968ccea05c57495c5013dfea
Gael Tikeng <Gealinho@yahoo.fr>
2023-12-23 16:44:19 +0100

    navbar almost done

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { BsSearch } from "react-icons/bs";
+import { GiShoppingCart } from "react-icons/gi";
+import { IoMdContact } from "react-icons/io";
+
+
 
 export default function Navbar() {
 
   return (
-    <div className="px-[80px] flex pt-5">
+    <div className="md:px-[80px] flex gap-4 pt-4 justify-between px-[20px] md:pt-5">
       <p className="text-2xl ">Code 4 all</p>
-      <div className="w-full sm:w-[300px] md:w-[60%] relative">
+      <div className="w-full sm:w-[300px] md:w-[60%] relative h-fit hidden md:flex bg-red-500">
         <BsSearch
           className="absolute left-0 top-0 ml-3 mt-3 text-gray-400 font-meduim"
           size={17}
@@ -18,6 +22,15 @@ export default function Navbar() {
           placeholder="To search"
         />
 
+      </div>
+      <div className="flex gap-4 ">
+        <GiShoppingCart
+          size="30"
+          className="my-auto"
+        />
+        <div className="md:hidden my-auto ">
+          <IoMdContact size="30"/>
+        </div>
       </div>
 
     </div>
