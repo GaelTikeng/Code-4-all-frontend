60f25d533b9a7fa7efe5d378edea9179b1339fb7
Gael Tikeng <Gealinho@yahoo.fr>
2024-01-08 06:42:42 +0100

    build error

--- src/utiles/function.ts
+++ src/utiles/function.ts
@@ -1,6 +1,8 @@
-import { Code, Test } from "../../types"
+import { Code } from "../../types"
 
 export const totalPrice = (codeCart: Code[] | undefined) => {
+  if (!codeCart) return 0
+  
   let sum: number = 0
   for (let i = 0; i < codeCart?.length; i++) {
     sum += codeCart[i]?.price
